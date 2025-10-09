f2af82529971c890c79dfa5e72539158412ded43
pragyangupta30 <pragyangupta30@gmail.com>
2025-10-09 14:42:24 +0530

    First Comment cicd demo

---
+++ train.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+
+joblib.dump(model, "model.pkl")
+print("✅ Model trained and saved as model.pkl")
