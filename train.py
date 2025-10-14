108eb4490ab65dfdbdd3402694a7a51ed1d65108
pragyangupta30 <pragyangupta30@gmail.com>
2025-10-14 16:36:47 +0530

    heeeelloo

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
+
+
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(
     iris.data, iris.target, test_size=0.2, random_state=42
@@ -12,5 +14,6 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
+print("✅ Model trained and saved as model.pkl")
 joblib.dump(model, "model.pkl")
 print("✅ Model trained and saved as model.pkl")
