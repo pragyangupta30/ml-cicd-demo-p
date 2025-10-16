48777c355ef3cb6ae8a4a9cc18ef0bcc80af2585
pragyangupta30 <pragyangupta30@gmail.com>
2025-10-16 12:19:03 +0530

    done

--- train.py
+++ train.py
@@ -14,6 +14,5 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
-print("✅ Model trained and saved as model.pkl")
 joblib.dump(model, "model.pkl")
 print("✅ Model trained and saved as model.pkl")
